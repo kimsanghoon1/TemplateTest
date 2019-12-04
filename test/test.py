5caa04583cd6d304b08bf440ed2465871dab8b5a
kimsanghoon1 <sanghoon01@uengine.org>
2019-12-04 15:42:35 +0900

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,1 +1,17 @@
-
+forEach: BoundedContext
+fileName: {{name}}World.py
+path: {{boundedContext}}/{{{options.packagePath}}}
+---
+print("BoundedContext: {{name}}");
+{{#aggregates}}
+print("Aggregate: {{name}}");
+{{#events}}
+print("event: {{name}}");
+{{/events}}
+{{#commands}}
+print("command: {{name}}");
+{{/commands}}
+{{#policies}}
+print("policy: {{name}}");
+{{/policies}}
+{{/aggregates}}
