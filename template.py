7b6e969a081dd30f27d1ef98b7ce696c48cf3a82
kimsanghoon1 <sanghoon01@uengine.org>
2019-12-04 12:23:55 +0900

    Create template.py

---
+++ template.py
@@ -0,0 +1,18 @@
+
+forEach: BoundedContext
+fileName: {{name}}World.py
+path: {{boundedContext}}/{{{options.packagePath}}}
+---
+print("BoundedContext: {{name}}");
+{{#aggregates}}
+print("Aggregate: {{name}}");
+{{#events}}
+print("event: {{name}}");
+{{/events}}
+{{#commands}}
+print("command: {{name}}");
+{{/commands}}
+{{#policies}}
+print("policy: {{name}}");
+{{/policies}}
+{{/aggregates}}
